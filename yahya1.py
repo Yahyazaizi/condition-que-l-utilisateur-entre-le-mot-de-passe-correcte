11c6f929c557a25b8ad34fc73d33cba24b5c6586
yahya zaizi <146640218+Yahyazaizi@users.noreply.github.com>
2023-10-18 18:43:07 +0200

    Add files via upload

---
+++ yahya1.py
@@ -0,0 +1,15 @@
+n="bonjour"
+m=input("entre le mot depasse correcte")
+e="DEV@2023"
+
+if m==e :
+    print(n)
+
+else:
+    for i in range(1,4,1):
+        novo = input("entre le mot depasse correcte")
+        print("entre le mot depasse correcte",novo)
+        novo=n
+print(n)
+
+
